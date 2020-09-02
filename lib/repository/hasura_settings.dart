d71a518fa4d8ea844cca87a9857043ee8b23e683
Ygor Teles <ygor.teles1994@gmail.com>
2020-09-02 16:41:40 -0300

    Delete hasura_settings.dart

--- lib/repository/hasura_settings.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:hasura_connect/hasura_connect.dart';
-
-class HasuraSettings extends GetxController{
-  static HasuraSettings get to => Get.find();
-
-  HasuraConnect connect = HasuraConnect("https://singular-toucan-38.hasura.app/v1/graphql");
-
-}
